9c301c0cc7dd7ce81611ee9a19ed369083d0c6d9
Angel <angeleduardoed@gmail.com>
2025-08-11 16:52:58 -0400

    Ejercicios: Resolviendo ejercicios de loops 2

--- Basic/15-loops-exercises.js
+++ Basic/15-loops-exercises.js
@@ -66,14 +66,89 @@ for (nombre of myArray) {
 }
 
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
-let myCadena
+console.log("---------------------- 5 ----------------------")
+let myCadena = "Esta es una cadena de texto donde se contaran las vocales"
+let vocales = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+let countVocales =0
+
+for (let cadena of myCadena) {
+    for (let vocal of vocales)
+        if (cadena == vocal) {
+            countVocales++
+            console.log(`vocal cadena:${cadena} vocal:${vocal} = ${countVocales}`)  
+        }
+}
+
+console.log(countVocales)
+
+let myCadena2 = "Escribe un bucle que cuente el número de vocales en una cadena de texto"
+let countVocales2 = 0
+const vocales2 = "aeiouáéíóú"
+const myCadenaMinuscula = myCadena2.toLowerCase()
+
+for (let i=0; i < myCadena2.length; i++) {
+    let caracter = myCadenaMinuscula[i]
+
+    if (vocales2.includes(caracter)) {
+        countVocales2++
+    }
+}
+
+console.log(`La cantidad de vocal(es) es: ${countVocales2} `)
 
 // 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
+console.log("---------------------- 6 ----------------------")
+const myArrayNumbers = [5, 7, 3, 2, 9]  // el resultado deberia ser 1.890
+let resultado = 1 // si lo hago con cero me da como resultado 0
+
+for (let array of myArrayNumbers) {
+    resultado *= array
+}
+
+console.log(`la multiplicación de los número ${myArrayNumbers.join("x")} es: ${resultado}`)
 
 // 7. Escribe un bucle que imprima la tabla de multiplicar del 5
+console.log("---------------------- 7 ----------------------")
+const numbers = 5
+let result = 0
+
+console.log(`------ Tabla Multiplicación del ${numbers} con for ------`)
+for (i = 1; i <= 10; i++) {
+    result = i * numbers
+    console.log(`${i} x ${numbers} = ${result}`)
+}
+
+console.log(`------ Tabla Multiplicación del ${numbers} con while ------`)
+let count = 1
+result = 0
+
+while (count <= 10) {
+    result = count * numbers
+    console.log(`${count} x ${numbers} = ${result}`)
+    count++
+}
 
 // 8. Usa un bucle para invertir una cadena de texto
+console.log("---------------------- 8 ----------------------")
+const myStringText = "Esto es una cadena de texto"
+let myStringTextInvert = ""
+
+for (i = myStringText.length - 1; i >= 0; i--) {
+    myStringTextInvert += myStringText[i]
+}
+
+console.log(`Mi cadena de texto invertida es: ${myStringTextInvert}`)
 
 // 9. Usa un bucle para generar los primeros 10 números de la secuencia de Fibonacci
+console.log("---------------------- 9 ----------------------")
+let fibonacciResult = ""
+let fibonacci = 0
+
+for (i = 2; i < 10; i++) {
+    fibonacci = i-2 + i-1
+    console.log(fibonacci)
+}
+
+console.log(fibonacci)
 
 // 10. Dado un array de números, usa un bucle para crear un nuevo array que contenga solo los números mayores a 10
