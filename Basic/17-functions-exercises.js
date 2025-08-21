651442d7ad662f793f77ccf3b9fb1eb13cf64acd
Angel <angeleduardoed@gmail.com>
2025-08-21 14:48:53 -0400

    Ejercicios: Resolviendo ejercicios de funciones (terminado)

--- Basic/17-functions-exercises.js
+++ Basic/17-functions-exercises.js
@@ -89,12 +89,86 @@ function numeroPrimo(num) {
 }
 
 console.log(numeroPrimo(373))
+
 // 6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
+console.log('-------------------- 6 ---------------------------')
+const commonElement = (myArray1, myArray2) => {
+    return myArray1.filter(element => myArray2.includes(element))
+}
+
+const array1 = ["casa", 1, 15, "mapa", 100, 102]
+const array2 = [0, 1.5, "mapa", 102, 105, 200]
+
+console.log(commonElement(array1, array2))
 
 // 7. Crea una función que reciba un array de números y devuelva la suma de todos los números pares
+console.log('-------------------- 7 ---------------------------')
+function sumPar(arrayNum) {
+    const arrayPar = arrayNum.filter(par => par % 2 === 0)
+    let sumElement = 0
+    arrayPar.forEach((sum) => {
+        sumElement += sum
+    })
+    return sumElement
+}
+
+const sumaPares = (array) => {
+    let suma = 0
+    for (spar of array) {
+        if (spar % 2 === 0) {
+            suma += spar
+        }
+    }
+    return suma
+}
+
+const sumandoPares = function(array) {
+    let sumaPar = 0
+    for (let i=0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            sumaPar += array[i]
+        }
+    }
+    return sumaPar
+}
+
+const array = [1, 0, 20, 15, 31, 100, 27, 2, 2**2]
+console.log(`Funcioón con filter de array: ${sumPar(array)}`)
+console.log(`Función con flecha: ${sumaPares(array)}`)
+console.log(`Funcion anonima: ${sumandoPares(array)}`)
 
 // 8. Crea una función que reciba un array de números y devuelva un nuevo array con cada número elevado al cuadrado
+console.log('-------------------- 8 ---------------------------')
+const numAlCuadrado = (array) => {
+    let numCuadrado = []
+    for (num of array) {
+        numCuadrado.push(num ** 2)
+    }
+    return numCuadrado
+}
 
+console.log(numAlCuadrado(array))
 // 9. Crea una función que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden inverso
+console.log('-------------------- 9 ---------------------------')
+const strtInverso = function(str) {
+    const wordArray = str.split(" ")
+    const ReverseWordArray = wordArray.reverse()
+    const reverseWord = ReverseWordArray.join(" ")
+    return reverseWord
+}
+
+const myString = "Esto es una cadena de texto"
+console.log(myString)
+console.log(strtInverso(myString))
 
 // 10. Crea una función que calcule el factorial de un número dado
+console.log('-------------------- 10 ---------------------------')
+const factorial = (num) => {
+    let acumula = 1
+    for (let i=1; i <= num; i++) {
+        acumula *=i
+    }
+    return acumula
+}
+
+console.log(factorial(7))
