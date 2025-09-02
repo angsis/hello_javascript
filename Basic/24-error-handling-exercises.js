5c905556ab1b58ecf633029fee8197ee513c08a9
Angel <angeleduardoed@gmail.com>
2025-09-02 15:49:44 -0400

    Ejercicios: de la clase No-24 sin completar-3

--- Basic/24-error-handling-exercises.js
+++ Basic/24-error-handling-exercises.js
@@ -32,8 +32,8 @@ const areaRectangulo = (x, y) => {
     if (typeof x !== "number" || typeof y !== "number") {
         throw new Error("Debe ingresar solo Números")
     }
-    return x * y 
-} 
+    return x * y
+}
 
 // 5. Lanza una excepción personalizada
 console.log("---------------------- 5 -------------------------")
@@ -52,13 +52,13 @@ const aceleracion = (x1, x2, t1, t2) => {
     if (x2 - x1 < 0 || t2 - t1 <= 0) {
         throw new Error("Error division entre 0")
     }
-    return (x2 - x1) / (t2 - t1) 
-} 
+    return (x2 - x1) / (t2 - t1)
+}
 
 // 7. Captura varias excepciones en un mismo try-catch
 console.log("---------------------- 7 -------------------------")
 try {
-    console.log(`La aceleración es: ${aceleracion("0", 1000, 0, 10)}`)
+    console.log(`La aceleración es: ${aceleracion(0, 1000, 0, 10)}`)
 } catch (error) {
     if (error instanceof TypeError) {
         console.log("Se ha producido un error de Tipo: ", error.message)
@@ -68,7 +68,27 @@ try {
 }
 
 // 8. Crea un bucle que intente transformar a float cada valor y capture y muestre los errores
+console.log("---------------------- 8 -------------------------")
+const myArray = ["123.45", "hola", "99.9", "texto100", "50", "5", "20", "20 / 3"];
+
+for (let i = 0; i < myArray.length; i++) {
+
+    try {
+        if (isNaN(myArray[i])) {
+            throw new Error("El dato no es un número")
+        }
+
+        let value = myArray[i]
+        let floatValue = parseFloat(value)
+
+        console.log(`El valor ${value} (${typeof value}) se transformó a: ${floatValue} (${typeof floatValue})`)
+
+    } catch (error) {
+        console.log("Ha ocurrido un error: ", error.message)
+    }
+}
 
 // 9. Crea una función que verifique si un objeto tiene una propiedad específica y lance una excepción personalizada
+console.log("---------------------- 9 -------------------------")
 
 // 10. Crea una función que realice reintentos en caso de error hasta un máximo de 10
