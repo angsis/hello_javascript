bcb129b21e9ee22d8076153d5f75e17945eac18c
Angel <angeleduardoed@gmail.com>
2025-09-12 16:47:50 -0300

    Ejercicio: clase 00 Intermedio - 02

--- intermediate/01-advanced-functions-exercises.js
+++ intermediate/01-advanced-functions-exercises.js
@@ -11,7 +11,7 @@ greetAngel()
 
 // 2. Implementa una función currificada que multiplique 3 números
 console.log("------------------------  2  -------------------------")
-function multiplyCurry (num1) {
+function multiplyCurry(num1) {
     return function (num2) {
         return function (num3) {
             return num1 * num2 * num3
@@ -26,22 +26,103 @@ const multNum3 = multNum2(1)
 console.log(multNum3)
 
 // 3. Desarrolla una función recursiva que calcule la potencia de un número elevado a un exponente
+console.log("------------------------  3  -------------------------")
 function potencia(num, exponente) {
     if (exponente < 1) {
         return 1
     }
-    return potencia(num, exponente-1) * num
+    return potencia(num, exponente - 1) * num
 }
 
-console.log(potencia(3,2))
+console.log(potencia(3, 2))
 
 // 4. Crea una función createCounter() que reciba un valor inicial y retorne un objeto con métodos para increment(), decrement() y getValue(), utilizando un closure para mantener el estado
+console.log("------------------------  4  -------------------------")
+function createCounter(valInitial) {
+    let counter = valInitial
+    return funcsCounter = {
+        getValue: function () {
+            return counter
+        },
+        increment: function () {
+            return counter++
+        },
+        decrement: function () {
+            return counter--
+        }
+    }
+}
+const counter = createCounter(10)
+counter.decrement()
+counter.decrement()
+counter.decrement()
+counter.increment()
+console.log(counter.getValue())
+counter.increment()
+console.log(counter.getValue())
+counter.decrement()
+console.log(counter.getValue())
+console.log(counter)
 
 // 5. Crea una función sumManyTimes(multiplier, ...numbers) que primero sume todos los números (usando parámetros Rest) y luego multiplique el resultado por multiplier
+console.log("------------------------  5  -------------------------")
+function sumManyTimes(multiplier, ...numbers) {
+    let sumNumbers = 0
+    for (i = 0; i < numbers.length; i++) {
+        sumNumbers += numbers[i]
+    }
+    return sumNumbers * multiplier
+}
+let myArray = [10, 10, 10, 10, 10, 200, 500, 20]
+console.log(sumManyTimes(10, ...myArray))
+console.log(sumManyTimes(10, 10, 10, 10, 10, 10, 200, 500, 20))
 
 // 6. Crea un Callback que se invoque con el resultado de la suma de todos los números que se le pasan a una función
+console.log("------------------------  6  -------------------------")
+function sumNumbers(...numbers) {
+    let sumNumbers = 0
+    for (i = 0; i < numbers.length; i++) {
+        sumNumbers += numbers[i]
+    }
+    return sumNumbers
+}
+
+function sum(a, b, c) {
+    return a + b + c
+}
+
+function processData(data, callback) {
+    const result = sumNumbers(...data)
+    return callback(result)
+}
+
+function processData2(data, callback) {
+    const result = sum(...data)
+    return callback(result)
+}
+
+function getResult(resultSuma) {
+    return console.log(`El resultado de la suma es: ${resultSuma}`)
+}
+
+processData([10, 20, 30, 40, 50], getResult)
+processData([2, 3, 5], getResult)
 
 // 7. Desarrolla una función parcial
+console.log("------------------------  7  -------------------------")
+function person(name, age, height, weight) {
+    return `Nombre: ${name}, Edad: ${age}, Altura: ${height}, Peso: ${weight}`
+}
+
+function greetingComplete(name) {
+    return function (age = 0, height = 0, weight = 0) {
+        return person(name, age, height, weight)
+    }
+}
+
+const greet = greetingComplete("Angel Eduardo")
+console.log(greet(50, 1.80, 92))
+console.log(greet())
 
 // 8. Implementa un ejemplo que haga uso de Spread
 
